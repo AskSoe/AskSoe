c72ccb091017fac790728ed133462b946e10d974
Daniel Shaw <danielshaw@mac.lan>
2025-07-09 16:58:44 +1000

    Dynamically import vite only in development to avoid production dependency error

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, log } from "./vite";
+import { log } from "./vite";
 import path from "path";
 
 const app = express();
@@ -68,6 +68,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
   }
 
